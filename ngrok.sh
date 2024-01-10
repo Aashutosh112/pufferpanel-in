f5d81a74723d8e5f5f9852c59b92f1414ca04ab0
Aashutosh112 <155889071+Aashutosh112@users.noreply.github.com>
2024-01-10 19:49:48 +0545

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2aeX4cs4MTzYimDVm9pRpfMeg4R_7aVc58JpynJXGBZL4HTyg #Put Yours here
+./ngrok config add-authtoken 2alZ7n9Vu2yaGB8QNzBPBXBY5Jb_6ZJufeN5UpHLpRfq4giAv
 ./ngrok http 8080
